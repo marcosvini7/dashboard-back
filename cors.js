ed035659c41b018119fd248ad356a19c7e5b4f95
= <marcos.sousa462@gmail.com>
2024-11-25 22:27:44 -0300

    ajustes

--- cors.js
+++ cors.js
@@ -1,14 +1,8 @@
-const cors = require('cors')
+const cors = require('cors');
 
-// Apenas requisições das origens definidas são permitidas
-const allowedOrigins = [process.env.FRONT_ORIGIN || 'http://localhost:8080']
-const corsOptions = { origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
+// Permite requisições de todas as origens
+const corsOptions = {
+  origin: '*'
+};
 
-module.exports = cors(corsOptions)
+module.exports = cors(corsOptions);
